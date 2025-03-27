be9061d6d9ac93b0d4517a73f13a49906089facc
dsevertsev <dsevertsov@strongteam.tech>
2025-03-27 18:57:10 +0300

    рабочий конфиг сервер

--- src/main/java/ru/aza954/configservertelegramhihi/ConfigServerTelegramhihiApplication.java
+++ src/main/java/ru/aza954/configservertelegramhihi/ConfigServerTelegramhihiApplication.java
@@ -2,7 +2,9 @@ package ru.aza954.configservertelegramhihi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerTelegramhihiApplication {
 
