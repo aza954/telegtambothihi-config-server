229dedfb40dba7ee65d9ad8df44b806887216bd8
dsevertsev <dsevertsov@strongteam.tech>
2025-03-27 18:13:09 +0300

    init

---
+++ src/main/java/ru/aza954/configservertelegramhihi/ConfigServerTelegramhihiApplication.java
@@ -0,0 +1,13 @@
+package ru.aza954.configservertelegramhihi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerTelegramhihiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerTelegramhihiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/aza954/configservertelegramhihi/ConfigServerTelegramhihiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aza954.configservertelegramhihi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTelegramhihiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
